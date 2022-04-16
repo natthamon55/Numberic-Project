934ad3c32d71d2f0e9946c4493db07ea25acf0c3
natthamon55 <natthamon2012@hotmail.com>
2022-04-16 21:02:34 +0700

    add timer

--- first_ode.py
+++ first_ode.py
@@ -2,10 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
-
 #Solve differential equation. There are two main solvers in scipy
 from scipy.integrate import odeint #Pretty classic, uses a particular solve called lsoda from the FORTRAN library odepack
 from scipy.integrate import solve_ivp #More customizable, can choose from a list of possible solvers
+import time
 
 def f(t, v):
   dydt = 3*v**2 - 5 #y' = 3v**2-5
@@ -14,14 +14,20 @@ def f(t, v):
 v0 = 0
 t = np.linspace(0, 1, 100) #solve 100 ครั้งระหว่าง 0-1 เพื่อต้องการมา plot solution
 
+start_odeint = time.time()
 sol_m1 = odeint(f, y0=v0, t=t, tfirst=True) #y0 คือ initial condotion เป็นพารามิเตอร์
-sol_m2 = solve_ivp(f, t_span=(0,max(t)), y0=[v0], t_eval=t) #t_span คือเราจะใช้เวลาในการsolve เท่าไหร่ระหว่าง0-1
-
 v_sol_m1 = sol_m1.T[0]
-v_sol_m2 = sol_m2.y[0]
+end_odeint = time.time()
 
+start_ivp = time.time()
+sol_m2 = solve_ivp(f, t_span=(0,max(t)), y0=[v0], t_eval=t) #t_span คือเราจะใช้เวลาในการsolve เท่าไหร่ระหว่าง0-1
+v_sol_m2 = sol_m2.y[0]
+end_ivp = time.time()
+
+print("Odeint uses time : {} s".format(end_odeint-start_odeint))
 print('odeint ans : ',v_sol_m1)
 print('-------------------------------------------------------------------------------------')
+print("solve_ivp uses time : {} s".format(end_ivp-start_ivp))
 print('solve_ivp ans  : ',v_sol_m2)
 
 plt.plot(t, v_sol_m1)
