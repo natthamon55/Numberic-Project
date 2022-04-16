d1d525b67e02d10aa6fcda11dc99953dd8acbc77
natthamon55 <natthamon2012@hotmail.com>
2022-04-16 22:34:25 +0700

    add plot error

--- first_ode.py
+++ first_ode.py
@@ -11,11 +11,11 @@ def f(t, v):
   dydt = 3*v**2 - 5 #y' = 3v**2-5
   return dydt
   
-v0 = 0
-t = np.linspace(0, 1, 100) #solve 100 ครั้งระหว่าง 0-1 เพื่อต้องการมา plot solution
+v0 = 0 #the initial condition
+t = np.linspace(0, 1, 100) #solve 100 ครั้งระหว่าง 0-10 เพื่อต้องการมา plot solution
 
 start_odeint = time.time()
-sol_m1 = odeint(f, y0=v0, t=t, tfirst=True) #y0 คือ initial condotion เป็นพารามิเตอร์
+sol_m1 = odeint(f, y0=v0, t=t, tfirst=True) #y0 คือ initial condition เป็นพารามิเตอร์
 v_sol_m1 = sol_m1.T[0]
 end_odeint = time.time()
 
@@ -25,13 +25,27 @@ v_sol_m2 = sol_m2.y[0]
 end_ivp = time.time()
 
 print("Odeint uses time : {} s".format(end_odeint-start_odeint))
+print("solve_ivp uses time : {} s".format(end_ivp-start_ivp))
+
+
 print('odeint ans : ',v_sol_m1)
 print('-------------------------------------------------------------------------------------')
-print("solve_ivp uses time : {} s".format(end_ivp-start_ivp))
 print('solve_ivp ans  : ',v_sol_m2)
 
-plt.plot(t, v_sol_m1)
-plt.plot(t, v_sol_m2, '--')
+#plot 2 methods
+plt.plot(t, v_sol_m1, label="odeint")
+plt.plot(t, v_sol_m2, '+',label="solve_ivp")
 plt.ylabel('y(t)', fontsize=22)
 plt.xlabel('t', fontsize=22)
+plt.legend()
 plt.show()
+
+#plot error
+err = np.abs(v_sol_m1-v_sol_m2)
+plt.ylabel("Error")
+plt.semilogy(t,err)
+plt.xlabel("v")
+plt.plot(t, err)
+plt.title("Error in ode")
+plt.show()
+
